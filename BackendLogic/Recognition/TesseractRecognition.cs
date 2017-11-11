60959eed0deab1c7e5fde241242fb018ec959ec1
Kasparas Taminskas <kasparas.taminskas@gmail.com>
2017-11-11 15:27:21 +0200

    Implemented Register, Login Controllers and Disabled GUI and Logic from Building.

--- BackendLogic/Recognition/TesseractRecognition.cs
+++ BackendLogic/Recognition/TesseractRecognition.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -88,3 +88,4 @@ namespace Logic
     }
 }
 
+*/
---
+++ WEB/Models/UserAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WEB.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required(ErrorMessage = "Username is required!")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Name is required!")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required!")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Password Confirmation is required!")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
