c9e30a8af825cd3d5f92eb08f938f9113cf1a7ff
ereiv <e.reivytyte@gmail.com>
2017-12-11 16:11:41 +0200

    Minor

--- WEB/Controllers/ViewDiscountController.cs
+++ WEB/Controllers/ViewDiscountController.cs
@@ -22,5 +22,19 @@ namespace WEB.Controllers
             var discounts = db.discounts.Include("Shop");
             return View(discounts.Where(x => x.Date >= DateTime.Today).ToList());
         }
+        [HttpPost]
+        public ActionResult Search(Discounts discounts)
+        {
+            string Shop = Request["Search"];
+            var discount = db.discounts.Where(x => x.Shop.ShopName == Shop).ToList();
+            return View("ViewDiscount", discount);
+        }
+        [HttpPost]
+        public ActionResult SearchToday(Discounts discounts)
+        {
+            string Shop = Request["SearchToday"];
+            var discount = db.discounts.Where(x => x.Shop.ShopName == Shop).Where(x => x.Date >= DateTime.Today).ToList();
+            return View("ViewDiscountToday", discount);
+        }
     }
 }
